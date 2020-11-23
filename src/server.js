be2febaa8060ca24639e1118691911d7bec7c459
Vladi Shunturov <shunturov@gmail.com>
2020-11-22 19:16:44 -0800

    Added Webhooks async functionality

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ require('dotenv').config({ path: '../.env' })
 //Initiate Stripe
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(express.static("../public"));
-app.use(express.json());
+//app.use(express.json());
 
 //Permit requests across any origin
 app.use((_, res, next) => {
@@ -20,11 +20,35 @@ app.use((_, res, next) => {
     next()
 })
 
+//Obtain request raw body for the purposes of verifying webhook signatures
+app.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function(req, res, buf) {
+      if (req.originalUrl.startsWith("/webhook")) {
+        req.rawBody = buf.toString();
+      }
+    }
+  })
+);
+
+
+/*
+//Checkout page
+app.get("/checkout", (req, res) => {
+    // Display checkout page
+    const path = resolve("../public/index.html");
+    res.sendFile(path);
+  });
+  */
+  
+
 const calculateOrderAmount = items => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
-  return 1400;
+  return 95000;
 };
 app.post("/create-payment-intent", async (req, res) => {
   const { items } = req.body;
@@ -34,8 +58,50 @@ app.post("/create-payment-intent", async (req, res) => {
     currency: "usd"
   });
   res.send({
+   //publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     clientSecret: paymentIntent.client_secret
   });
 });
+
+// Expose a endpoint as a webhook handler for asynchronous events.
+app.post("/webhook", async (req, res) => {
+    let data, eventType;
+    console.log("We got a webhook event!");
+
+    // Check if webhook signing is configured.
+    if (process.env.STRIPE_WEBHOOK_SECRET) {
+      // Retrieve the event by verifying the signature using the raw body and secret.
+      let event;
+      let signature = req.headers["stripe-signature"];
+      try {
+        event = stripe.webhooks.constructEvent(
+          req.rawBody,
+          signature,
+          process.env.STRIPE_WEBHOOK_SECRET
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`);
+        return res.sendStatus(400);
+      }
+      data = event.data;
+      eventType = event.type;
+      console.log("Webhook signature successfully verified!");
+    } else {
+      // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+      // we can retrieve the event data directly from the request body.
+      data = req.body.data;
+      eventType = req.body.type;
+    }
+  
+    if (eventType === "payment_intent.succeeded") {
+      // Funds have been captured
+      // Fulfill any orders, e-mail receipts, etc
+      // To cancel the payment after capture you will need to issue a Refund (https://stripe.com/docs/api/refunds)
+      console.log("💰 Payment captured!");
+    } else if (eventType === "payment_intent.payment_failed") {
+      console.log("❌ Payment failed.");
+    }
+    res.sendStatus(200);
+  });
 
 app.listen(4242, () => console.log('Node server listening on port 4242!'));
